cf336e4333e8166ddd3b18c47d33b87d118ee9b2
Krupal Patel <krupal.patel@Outlook.in>
2024-05-19 11:22:38 +0530

    Added lambdatest using playwright automation framework.

--- tests/functional/scenario1.spec.ts
+++ tests/functional/scenario1.spec.ts
@@ -10,17 +10,3 @@ test(`@lambdatest Test Scenario 1`, async ({ interactionsPage, webActions }) => 
     await webActions.verifyContentOnPage(message, 'message') // Click on Interactions Icon identified via text selector
     // await interactionsPage.verifyDragandDrop();
 });
-
-// test(`@lambdatest Test Scenario 2`, async ({ loginPage, interactionsPage, webActions }) => {
-//     await webActions.navigateToURL();
-//     await webActions.clickByText('Drag & Drop Sliders'); // Click on Interactions Icon identified via text selector
-//     await interactionsPage.moveSlider(15,95);
-//     await webActions.delay(5000)
-//     await webActions.verifyContentOnPage('95', 'rangeSuccess') // Click on Interactions Icon identified via text selector
-// });
-
-// test(`@lambdatest Test Scenario 3`, async ({ loginPage, interactionsPage, webActions }) => {
-//     await webActions.navigateToURL();
-//     await webActions.clickByText('Input Form Submit');
-//     await interactionsPage.fillInForm()
-// });
